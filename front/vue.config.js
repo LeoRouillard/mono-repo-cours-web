76df62136aefded6cca49efea87af1abae1ed210
Léo ROUILLARD <leorouillard@gmail.com>
2021-06-17 16:57:30 +0200

    Revert "fix"
    This reverts commit db0f8542299c11620bb74bb038c0d2b3e36d1791.

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,15 +1,7 @@
 const path = require('path')
-
 module.exports = {
-    devServer: { 
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8000',
-                changeOrigin: true
-            }
-        }
-    },
-    outputDir: path.resolve(__dirname,'../laravel/public'),
-    publicPath:'./dist',
+    devServer: { proxy: 'http://localhost:8000' },
+    outputDir: path.resolve(__dirname,'../laravel/public/dist'),
+    publicPath:'/dist',
     indexPath: path.resolve(__dirname,'../laravel/resources/views/index.html')
-}
+  }
