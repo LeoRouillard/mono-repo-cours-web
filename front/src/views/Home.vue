db0f8542299c11620bb74bb038c0d2b3e36d1791
Léo ROUILLARD <leorouillard@gmail.com>
2021-06-17 16:57:27 +0200

    fix

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -2,17 +2,31 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ data }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      data: {}
+    }
+  },
+  created () {
+    console.log("test")
+    axios.get('/api/test')
+      .then((response) => {
+        this.data = response.data
+      })
   }
 }
 </script>
--- front/vue.config.js
+++ front/vue.config.js
@@ -1,7 +1,15 @@
 const path = require('path')
+
 module.exports = {
-    devServer: { proxy: 'http://localhost:8000' },
-    outputDir: path.resolve(__dirname,'../laravel/public/dist'),
-    publicPath:'/dist',
+    devServer: { 
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true
+            }
+        }
+    },
+    outputDir: path.resolve(__dirname,'../laravel/public'),
+    publicPath:'./dist',
     indexPath: path.resolve(__dirname,'../laravel/resources/views/index.html')
-  }
+}
